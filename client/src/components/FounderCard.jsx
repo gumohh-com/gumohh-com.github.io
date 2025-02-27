41feb83a681e1a1dc551612546e99e49f1090885
pkradiator <kalalpriyanshu7@gmail.com>
2025-02-27 19:59:06 +0530

    Created Founder Sections

---
+++ client/src/components/FounderCard.jsx
@@ -0,0 +1,13 @@
+const FounderCard = ({info}) => {
+    return (
+        <div className="flex flex-col items-center p-4 mt-8 text-white">
+            <img src={info.img} alt="Our Founder Sandeep Patel" className="w-80 h-auto"/>
+            <h4 className="p-4 text-xl">{info.name}</h4>
+            <p className="w-80 text-center">
+                {info.about}
+            </p>
+        </div>
+    )
+}
+
+export default FounderCard
