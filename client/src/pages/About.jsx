028b04cfc5b7f90b4a25b4b3accb6f8f6c9067fc
vardhman-dewasi <vardhman738@gmail.com>
2025-03-02 17:50:18 +0530

    Gallery and about us Updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import FounderCard from "../components/FounderCard";
+import FounderSection from "../components/FounderSection";
 const About = () => {
-  return <div>About Us Page</div>;
+  return <div className="bg-black">
+    <div className="pt-20">
+    <FounderSection/>
+    </div>
+    </div>
 };
 
 export default About;
--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
 
 const Gallery = () => {
-  return <div>Gallery Page</div>;
+  const images = Array.from({ length: 8 }, (_, i) => `/g${i + 1}.jpeg`);
+
+  return (
+    <div className="bg-black p-4 grid grid-cols-2 md:grid-cols-4 gap-4 pt-20">
+      {images.map((src, index) => (
+        <div key={index} className="overflow-hidden rounded-2xl shadow-md">
+          <img src={src} alt={`Gallery image ${index + 1}`} className="w-full h-auto object-cover" />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Gallery;
