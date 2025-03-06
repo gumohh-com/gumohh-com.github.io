23fab5ad2804cd6300c69e4dcf63c30dd467f373
pkradiator <kalalpriyanshu7@gmail.com>
2025-03-06 11:47:59 +0530

    Added links

--- client/src/components/FounderSection.jsx
+++ client/src/components/FounderSection.jsx
@@ -5,12 +5,14 @@ const founderList = [
     about:
       "The people person, our representative and co-founder. With extensive experience travelling across the India, Sandeep ensures that each trip we plan is the best it can be.",
     img: "/sandeep.jpeg",
+    insta: "https://www.instagram.com/sandeeppatel6905/",
   },
   {
     name: "Jaydev Singh Rao",
     about:
       "The tech/research guy and co-founder. Having a knack for solving problems, Jaydev works on the technological solutions needed to help us achieve our common vision",
     img: "/jaydev_kings_college-scaled.jpg",
+    insta: "https://www.instagram.com/jaydev.sr/",
   },
 ];
 const FounderSection = () => {
